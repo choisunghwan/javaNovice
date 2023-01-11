b2c3b7513365be5cb969149a8dafb1ba389847c0
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2023-01-11 23:46:59 +0900

    변수와 자료형

---
+++ src/chapter2/ByteVariable.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class ByteVariable {
+    public static void main(String[] args) {
+        byte bData = -128;
+        System.out.println(bData);
+
+// 오류발생 : -128~127
+//        byte bData2 = 128;
+//        System.out.println(bData2);
+
+    }
+}
---
+++ src/chapter2/VariableEx2.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class VariableEx2 {
+    public static void main(String[] args) {
+        // 8바이트 자료형
+        // 가장 큰 정수 자료형
+        // 숫자의 뒤에 L 또는 l 을 써서 long 형임을 표시해야함.
+        long num = 12345678900L;
+        int level = 50;
+    }
+
+}
