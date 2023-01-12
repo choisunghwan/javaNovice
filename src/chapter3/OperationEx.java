0c27e5f83e01c2fa2856bc72ed90ed8dfe619a40
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2023-01-12 21:29:29 +0900

    # 연산자 , 제어흐름

---
+++ src/chapter3/OperationEx.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+public class OperationEx {
+    public static void main(String[] args) {
+        int age = 24;
+        System.out.println("age = " + age);
+
+        int num = 10;
+        int num2 = -num;
+        System.out.println("num = " + num);
+        System.out.println("num2 = " + num2);
+
+        num = -num;
+        System.out.println("num = " + num);
+    }
+}
---
+++ src/chapter3/OperationEx2.java
@@ -0,0 +1,12 @@
+package chapter3;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        
+        int num = 10;
+        System.out.println("num++ = " + num++);
+        System.out.println("num = " + num);
+
+        System.out.println(3>5);
+    }
+}
